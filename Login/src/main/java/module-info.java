a4087951e889373537e13d389a7467308970aea6
shihanwj <shihanwei495@gmail.com>
2024-10-03 10:44:36 +0200

    Delete Login directory

--- Login/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module es.ieslosmontecillos.login {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens es.ieslosmontecillos.login to javafx.fxml;
-    exports es.ieslosmontecillos.login;
-}
